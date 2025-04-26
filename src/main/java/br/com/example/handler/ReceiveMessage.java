ce3cbaadc83401cc994662de905e5ff96ba0926f
Jonatas <delimajonatas875@gmail.com>
2025-04-26 09:59:40 -0300

    chore: controle de estado para o loop de ReceiveMessage para ser encerrado ou continuar rodando

--- src/main/java/br/com/example/handler/ReceiveMessage.java
+++ src/main/java/br/com/example/handler/ReceiveMessage.java
@@ -6,21 +6,27 @@ import java.io.IOException;
 
 public class ReceiveMessage extends Thread {
     private final ReaderInput readerInput;
+    private boolean running;
 
     public ReceiveMessage(ReaderInput readerInput) {
         this.readerInput = readerInput;
+        this.running = true;
     }
+
+    public void stopReceive() {
+        this.running = false;
+    }
+
 
     @Override
     public void run() {
-        boolean running = true;
-        while (running) {
+        while (this.running) {
             try {
                 if (this.readerInput.hasNexLine()) {
                     System.out.println(this.readerInput.readLine());
                 }
             } catch (IOException e) {
-                running = false;
+                this.running = false;
             }
         }
     }
