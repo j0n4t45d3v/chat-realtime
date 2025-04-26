141ac0a0140aa78881582ad54e80148e4d77aaa9
Jonatas <delimajonatas875@gmail.com>
2025-04-26 09:58:53 -0300

    chore: add commandos para o client

--- src/main/java/br/com/example/ChatClient.java
+++ src/main/java/br/com/example/ChatClient.java
@@ -7,10 +7,13 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
 
 public class ChatClient {
 
-    public static final String ESCAPE_CLEAN_TERMINAL = "\\u001b[H\\u001b[2J";
+    public static final String ESCAPE_CLEAN_TERMINAL = "\u001b[H\u001b[2J";
 
     public static void main(String[] args) throws IOException {
         try (Socket socket = new Socket("127.0.0.1", 8888)) {
@@ -22,15 +25,64 @@ public class ChatClient {
             receiveMessage.start();
 
             boolean isConnected = true;
+            System.out.println("Escreva /help para ver os comandos que existem no chat!");
+            Map<String, Consumer<String>> options = getClientCommand(sendToServe);
             while (isConnected) {
                 String text = clientInputReader.readLine();
-                System.out.println("> "+ text);
-                if (text.equals("/clean")) {
-                    System.out.println(ESCAPE_CLEAN_TERMINAL);
+                String command = text.split(" ")[0];
+                Consumer<String> commandExecute = options.getOrDefault(command, (String value) -> {
+                    System.out.printf("Comando %s não existe", value);
+                    showHelper();
+                });
+                commandExecute.accept(text);
+                if (text.equals("/quit")) {
+                    isConnected = false;
+                    receiveMessage.stopReceive();
                 }
-                sendToServe.write(text.getBytes(StandardCharsets.UTF_8));
-                sendToServe.flush();
             }
+        }
+    }
+
+    private static Map<String, Consumer<String>> getClientCommand(OutputStream sendToServe) {
+        Map<String, Consumer<String>> options = new HashMap<>();
+        options.put("/clean", text -> cleanScreen());
+        options.put("/help", text -> showHelper());
+        options.put("/quit", text -> {
+            text = text.substring(1).trim() + '\n';
+            sendMessage(sendToServe, text);
+        });
+        options.put("/msg", text -> {
+            System.out.println(text);
+            text = text.replace("/msg", "").trim() + '\n';
+            sendMessage(sendToServe, text);
+        });
+        return options;
+    }
+
+    public static void cleanScreen() {
+        System.out.print(ESCAPE_CLEAN_TERMINAL);
+        System.out.flush();
+    }
+
+    public static void showHelper() {
+        System.out.println();
+        System.out.println("\toptions:");
+        System.out.println();
+        System.out.println("\t/quit                 sai do chat.");
+        System.out.println("\t/clean                limpa o chat.");
+        System.out.println("\t/help                 mostra essa mensagem com os comandos.");
+        System.out.println("\t/join <canal>         mostra essa mensagem com os comandos.");
+        System.out.println("\t/create-group         mostra essa mensagem com os comandos.");
+        System.out.println("\t/msg [message]        use o prefixo '>' para poder enviar mensagens.");
+        System.out.println();
+    }
+
+    public static void sendMessage(OutputStream server, String text) {
+        try {
+            server.write(text.getBytes(StandardCharsets.UTF_8));
+            server.flush();
+        } catch (IOException e) {
+            System.out.println("Falha ao enviar a mensagem");
         }
     }
 }
