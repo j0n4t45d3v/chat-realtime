d9be8ec33a47e4a6aab21fa62d370e7109ea5f57
Jonatas <jonatas.melo@3lminformatica.com.br>
2025-05-09 13:25:21 -0300

    test: add validation if message writed in client output is valid

--- src/test/java/br/com/example/handler/ReceiveMessageTest.java
+++ src/test/java/br/com/example/handler/ReceiveMessageTest.java
@@ -1,29 +1,51 @@
 package br.com.example.handler;
 
 import br.com.example.utils.ReaderInput;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class ReceiveMessageTest {
 
+    private static final PrintStream mainOut = System.out;
+    private static final ByteArrayOutputStream bytesOutStream = new ByteArrayOutputStream();
+
+    @BeforeAll
+    static void setUp() {
+        System.setOut(new PrintStream(bytesOutStream));
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.setOut(mainOut);
+    }
+
     @Test
     @DisplayName("should receive messages in other threat when client is connected")
     void shouldReceiveMessagesInOtherThreatWhenClientISConnected() throws Exception {
+        final String mockMessage = "Test message";
         ReaderInput mockInput = Mockito.mock(ReaderInput.class);
 
         when(mockInput.hasNexLine())
                 .thenReturn(true)
                 .thenReturn(false);
 
-        when(mockInput.readLine()).thenReturn("Test message");
+        when(mockInput.readLine()).thenReturn(mockMessage);
 
         ReceiveMessage thread = new ReceiveMessage(mockInput);
         thread.start();
 
         thread.join(500);
+
+        assertEquals(
+                mockMessage + System.lineSeparator(),
+                bytesOutStream.toString()
+        );
 
         verify(mockInput, atLeastOnce()).readLine();
     }
